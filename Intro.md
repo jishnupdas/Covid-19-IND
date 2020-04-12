07bd806066d53a99c3c43aa61dc6a9a446c8de85
jishnupdas <jishnua511@gmail.com>
2020-04-13 00:13:01 +0530

    refactored code (OOP) minor mods, moved the detailed report away from Readme page

--- Intro.md
+++ Intro.md
@@ -1,6 +1,6 @@
 ---
 author: 
-    - Covid-19 India Statewise
+    - jishnupdas
 title:
     - Covid-19 India Statewise
 header-includes: |
@@ -9,9 +9,6 @@ header-includes: |
 
 # Covid-19 Statewise data for India
 
-### Credits : [Data Source https://github.com/datameet/covid19](https://github.com/datameet/covid19)
-### [jishnupdas](https://github.com/jishnupdas/Covid-19-IND)
----
 
 # This repo provides a simple visualisation for Covid-19 information for each of the indian states
 
@@ -28,4 +25,235 @@ Each state's data is visualized in three different plots
 # Summary 
 
 ![statewise numbers](plots/summary.png)
+
+
+# Report
+
+### Click [here to view the detailed statewise data and report](https://github.com/jishnupdas/Covid-19-IND/blob/master/report.pdf)
+---
+
+
+# Credits 
+
+### [Data Source https://github.com/datameet/covid19](https://github.com/datameet/covid19)
+### [jishnupdas](https://github.com/jishnupdas/Covid-19-IND)
+---
+
+
+# Maharashtra 
+
+\centering
+
+![](plots/mh.png){width=70%}
+
+
+# Delhi 
+
+\centering
+
+![](plots/dl.png){width=70%}
+
+
+# Tamil Nadu 
+
+\centering
+
+![](plots/tn.png){width=70%}
+
+
+# Rajasthan 
+
+\centering
+
+![](plots/rj.png){width=70%}
+
+
+# Madhya Pradesh 
+
+\centering
+
+![](plots/mp.png){width=70%}
+
+
+# Telangana 
+
+\centering
+
+![](plots/tg.png){width=70%}
+
+
+# Uttar Pradesh 
+
+\centering
+
+![](plots/up.png){width=70%}
+
+
+# Gujarat 
+
+\centering
+
+![](plots/gj.png){width=70%}
+
+
+# Andhra Pradesh 
+
+\centering
+
+![](plots/ap.png){width=70%}
+
+
+# Kerala 
+
+\centering
+
+![](plots/kl.png){width=70%}
+
+
+# Karnataka 
+
+\centering
+
+![](plots/ka.png){width=70%}
+
+
+# Jammu and Kashmir 
+
+\centering
+
+![](plots/jk.png){width=70%}
+
+
+# Haryana 
+
+\centering
+
+![](plots/hr.png){width=70%}
+
+
+# Punjab 
+
+\centering
+
+![](plots/pb.png){width=70%}
+
+
+# West Bengal 
+
+\centering
+
+![](plots/wb.png){width=70%}
+
+
+# Bihar 
+
+\centering
+
+![](plots/br.png){width=70%}
+
+
+# Odisha 
+
+\centering
+
+![](plots/or.png){width=70%}
+
+
+# Uttarakhand 
+
+\centering
+
+![](plots/ut.png){width=70%}
+
+
+# Himachal Pradesh 
+
+\centering
+
+![](plots/hp.png){width=70%}
+
+
+# Assam 
+
+\centering
+
+![](plots/as.png){width=70%}
+
+
+# Chhattisgarh 
+
+\centering
+
+![](plots/ct.png){width=70%}
+
+
+# Chandigarh 
+
+\centering
+
+![](plots/ch.png){width=70%}
+
+
+# Jharkhand 
+
+\centering
+
+![](plots/jh.png){width=70%}
+
+
+# Ladakh 
+
+\centering
+
+![](plots/la.png){width=70%}
+
+
+# Andaman and Nicobar Islands 
+
+\centering
+
+![](plots/an.png){width=70%}
+
+
+# Goa 
+
+\centering
+
+![](plots/ga.png){width=70%}
+
+
+# Puducherry 
+
+\centering
+
+![](plots/py.png){width=70%}
+
+
+# Manipur 
+
+\centering
+
+![](plots/mn.png){width=70%}
+
+
+# Tripura 
+
+\centering
+
+![](plots/tr.png){width=70%}
+
+
+# Arunachal Pradesh 
+
+\centering
+
+![](plots/ar.png){width=70%}
+
+
+# Mizoram 
+
+\centering
+
+![](plots/mz.png){width=70%}
+
 
--- cov_vis.py
+++ cov_vis.py
@@ -7,6 +7,7 @@ Created on Tue Apr  7 09:42:50 2020
 """
 import os
 import json
+import itertools
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -21,27 +22,14 @@ os.system('./data_update.sh')
 
 f = '../covid19/data/mohfw.json'
 
-#with open(f,'r') as file:
-#    data = '\n'.join([line for line in itertools.islice(file, 1, None)])
-
 data = pd.read_json(f) # reading the json file
 
 df = pd.io.json.json_normalize(data['rows']) # getting data from the json file
 # i know its a little convolutedted here (found the solution after 40 minutes of searching :P)
 
 df['value.report_time'] = pd.to_datetime(df['value.report_time'])
+
 #%%
-data_ind = pd.read_json('../covid19/data/all_totals.json', orient='records')
-
-#data_ind = pd.io.json.json_normalize(data_ind['rows'])
-#with open('../covid19/data/all_totals.json', 'r') as f:
-#    data = json.load(f)
-#df = pd.DataFrame(data)
-#%%
-
-states = list(set(df['value.state']))
-
-#states.sort()
 
 # setting up full names of states
 state_dict  = {
@@ -84,8 +72,9 @@ class State:
     """This class provides functions to filter, organise and visualize statewise data for Covid-19"""
 
     def __init__(self, df, state, db=None, st_abbr=None, time=None,
-                 conf=None, cure=None, deth=None, conf_count=None,
-                 cure_count=None, deth_count=None, fatality_rate=None,
+                 conf=None, cure=None, deth=None, growthrate=None,
+                 conf_count=None,cure_count=None, deth_count=None,
+                 fatality_rate=None,
                  daily_conf=None, daily_cure=None, daily_death=None):
 
         '''Initializing the values'''
@@ -114,6 +103,10 @@ class State:
         self.daily_cure    = [0]+[self.cure[i]-self.cure[i-1] for i in range(1,len(self.cure))]
         self.daily_death   = [0]+[self.deth[i]-self.deth[i-1] for i in range(1,len(self.deth))]
 
+        try:
+            self.growthrate = ((self.conf[-1]/self.conf[-2]) - 1)*100
+        except:
+            self.growthrate = 0
 
     def get_details(self):
 
@@ -126,137 +119,105 @@ class State:
                     }
         return details
 
-#%%
+    def plot_summary(self):
 
-def plot_summary(state):
+        plt.style.use('seaborn')
+        fig, ax = plt.subplots(3, 1,figsize=(8,8),sharex=True,
+                            gridspec_kw={'height_ratios': [2.1, 2.4, 1]})
 
-    Filter = df[(df['value.state'] == state)]
-    st = state
-    state  = state_dict[state] # getting the full form
+        ax[0].set_title(self.state.upper(),fontsize=20)
 
-    fl = Filter.loc[Filter.groupby(lambda x: Filter['value.report_time'][x].day)['value.report_time'].idxmax()]
-    fl = fl.sort_values(['value.report_time'])
+        ax[0].plot(self.time, self.conf, 'C0-o',lw=5,ms=10,
+                label='confirmed cases ({})'.format(self.conf_count))
 
-    time = fl['value.report_time']
-    conf = fl['value.confirmed']
-    cure = fl['value.cured']
-    deth = fl['value.death']
+        ax[0].plot(self.time, self.cure,'C1-p',lw=4,ms=10,
+                label='recovered cases ({})'.format(self.cure_count))
 
-    confirm = conf.max()
-    cured_c = cure.max()
-    deaths  = deth.max()
-    fatality_rate = deaths*100/confirm
+        ax[0].plot(self.time, self.deth,'C2-v',alpha=.6,
+                label='deaths ({})'.format(self.deth_count))
 
-    lst = list(conf)
-    daily_conf = [0]+[lst[i]-lst[i-1] for i in range(1,len(lst))]
+        ax[0].fill_between(self.time,self.cure,self.deth,facecolor='C1',alpha=0.2)
 
-    lst = list(cure)
-    daily_cure = [0]+[lst[i]-lst[i-1] for i in range(1,len(lst))]
+        ax[0].fill_between(self.time,self.deth,facecolor='C2',alpha=0.2,
+                        label='fatality rate ({:0.2f}%)'.format(self.fatality_rate))
 
-    lst = list(deth)
-    daily_deth = [0]+[lst[i]-lst[i-1] for i in range(1,len(lst))]
+        ax[0].fill_between(self.time,self.conf,self.cure,facecolor='C0',alpha=0.1,
+                        label='Growth rate ({:.2f}%)'.format(self.growthrate))
 
-    try:
-        growthrate = ((lst[-1]/lst[-2]) - 1)*100
-#        growthrate = ((daily_conf[-1]/daily_conf[-3])**.33 - 1)*100
-    except:
-        growthrate = 0
+    #    ax[0].set_xlabel('Date')
+        ax[0].set_ylabel('Numbers')
+        #ax[0].set_ylim(0, 800)
+
+        ax[0].legend(loc=2,fontsize=15,frameon=True,fancybox=True,
+                    framealpha=.7,facecolor='white', borderpad=1)
 
 
-    plt.style.use('seaborn')
-    fig, ax = plt.subplots(3, 1,figsize=(8,8),sharex=True,
-                           gridspec_kw={'height_ratios': [2.1, 2.4, 1]})
+        ax[1].set_title("Log scale")
 
-    ax[0].set_title(state.upper(),fontsize=20)
+        ax[1].plot(self.time,self.conf,'C0-o',lw=5,ms=10)
 
-    ax[0].plot(time, conf, 'C0-o',lw=5,ms=10,
-               label='confirmed cases ({})'.format(confirm))
+        ax[1].plot(self.time,self.cure,'C1-p',lw=4,ms=10)
 
-    ax[0].plot(time, cure,'C1-p',lw=4,ms=10,
-               label='recovered cases ({})'.format(cured_c))
+        ax[1].plot(self.time,self.deth,'C2-v',alpha=.6)
 
-    ax[0].plot(time, deth,'C2-v',alpha=.6,
-               label='deaths ({})'.format(deaths))
+        ax[1].fill_between(self.time,self.conf,self.cure,facecolor='C0',alpha=0.1)
+        ax[1].fill_between(self.time,self.cure,self.deth,facecolor='C1',alpha=0.2)
+        ax[1].fill_between(self.time,self.deth,facecolor='C2',alpha=0.2)
 
-    ax[0].fill_between(time,cure,deth,facecolor='C1',alpha=0.2)
-
-    ax[0].fill_between(time,deth,facecolor='C2',alpha=0.2,
-                       label='fatality rate ({:0.2f}%)'.format(fatality_rate))
-
-    ax[0].fill_between(time,conf,cure,facecolor='C0',alpha=0.1,
-                       label='Growth rate ({:.2f}%)'.format(growthrate))
-
-#    ax[0].set_xlabel('Date')
-    ax[0].set_ylabel('Numbers')
-    #ax[0].set_ylim(0, 800)
-    ax[0].set_xticks(['2020-03-15','2020-03-30','2020-04-14'])
-    ax[0].set_xlim('2020-03-08 12:00:00+0530','2020-04-13')
-
-    ax[0].legend(loc=2,fontsize=15,frameon=True,fancybox=True,
-                 framealpha=.7,facecolor='white', borderpad=1)
+    #    ax[1].set_xlabel('Date')
+        ax[1].set_ylabel('Numbers')
+        ax[1].set_yscale('symlog')
+        ax[1].set_yticks([10**i for i in range(4)])
+        ax[1].set_yticklabels(['{:2d}'.format(10**i) for i in range(4)])
+        ax[1].set_ylim(0,10**4)
 
 
-    ax[1].set_title("Log scale")
+        ax[2].set_title('Daily Cases')
 
-    ax[1].plot(time,conf,'C0-o',lw=5,ms=10,
-               label='confirmed cases ({})'.format(confirm))
+        ax[2].bar(self.time, self.daily_conf)
 
-    ax[1].plot(time,cure,'C1-p',lw=4,ms=10,
-               label='recovered cases ({})'.format(cured_c))
+        ax[2].set_xticks(['2020-03-15','2020-03-30','2020-04-14','2020-04-29'])
+        ax[2].set_xticklabels(['15 March','30 March','14 April'])
+        ax[2].set_xlim('2020-03-08','2020-04-18')
+        ax[2].set_xlabel('Date')
 
-    ax[1].plot(time,deth,'C2-v',alpha=.6,
-               label='deaths ({})'.format(deaths))
-
-    ax[1].fill_between(time,conf,cure,facecolor='C0',alpha=0.1)
-    ax[1].fill_between(time,cure,deth,facecolor='C1',alpha=0.2)
-    ax[1].fill_between(time,deth,facecolor='C2',alpha=0.2)
-
-#    ax[1].set_xlabel('Date')
-    ax[1].set_ylabel('Numbers')
-    ax[1].set_yscale('symlog')
-    ax[1].set_yticks([10**i for i in range(4)])
-    ax[1].set_yticklabels(['{:2d}'.format(10**i) for i in range(4)])
-    ax[1].set_ylim(0,10**4)
-
-    ax[1].set_xticks(['2020-03-15','2020-03-30','2020-04-14'])
-    ax[1].set_xlim('2020-03-08 12:00:00+0530','2020-04-14')
-
-
-    ax[2].set_title('Daily Cases')
-    ax[2].bar(time, daily_conf)#,daily_cure,daily_deth)
-    ax[2].set_xticks(['2020-03-15','2020-03-30','2020-04-14'])
-    ax[2].set_xticklabels(['15 March','30 March','14 April'])
-    ax[2].set_xlim('2020-03-08 12:00:00+0530','2020-04-14')
-    ax[2].set_xlabel('Date')
-
-    fig.tight_layout()
-    plt.savefig(f'plots/{st}.png',dpi=150)
-#    plt.show()
-    plt.close()
+        fig.tight_layout()
+        plt.savefig(f'plots/{st}.png',dpi=150)
+        plt.show()
+        plt.close()
 
 #%%
-#plot_summary('kl')
+states_list = list(set(df['value.state']))
 
 #%%
-details = []
-for st in states:
+
+state_objects = []
+
+for st in states_list:
     state_obj = State(df,st)
-    details.append(state_obj.get_details())
+    name      = {'object':state_obj}
+    detail    = state_obj.get_details()
+    detail.update(name)
 
-db = pd.DataFrame(details)
+    state_obj.plot_summary()
+
+    state_objects.append(detail)
+
+
+db = pd.DataFrame([dict(itertools.islice(state.items(), 5)) for state in state_objects])
 db = db.sort_values(['confirmed'])
 db['Total'] = db.confirmed+db.cured+db.deaths
 
+
+#%%
 cases  = list(db.confirmed)
 cured  = list(db.cured)
 death  = list(db.deaths)
 tot    = list(db.Total)
 
-
-#%%
 ax = db[['State','deaths', 'confirmed', 'cured']].plot(kind='barh',
                                    figsize=(10,16), width=.5, fontsize=13,
-                                   colors=['C2', 'C0', 'C1'], stacked=True)
+                                   color=['C2', 'C0', 'C1'], stacked=True)
 for i in range(len(tot)):
     ax.text(tot[i]+5,i-.15,'{:<4d}'.format(cases[i]), fontsize=12)
     ax.text(tot[i]+120,i+.05,'{}'.format(cured[i]), fontsize=10, color='g')
@@ -276,20 +237,42 @@ plt.close()
 
 
 #%%
-with open('Intro.md','r') as intro:
-    with open('README.md','w') as outfile:
+states_list = list(db.code)[::-1]
+
+with open('README.md','r') as intro:
+    with open('Intro.md','w') as outfile:
         outfile.write(intro.read())
-        for state in states:
-            plot_summary(state)
+        for state in states_list:
             st     = state
             state  = state_dict[state]
             outfile.write(f'# {state} \n\n\centering\n\n![](plots/{st}.png){{width=70%}}\n\n\n')
-#            outfile.write(f'# {state} \n\n\n![](plots/{st}.png)\n\n\n')
+    #            outfile.write(f'# {state} \n\n\n![](plots/{st}.png)\n\n\n')
 
-os.system('pandoc README.md -t beamer -o report.pdf')
+os.system('pandoc Intro.md -t beamer -o report.pdf')
 
 #%%
 '''test code'''
+
+#%%
+
+#self = State(df,'dl')
+#self.get_details()
+#
+#self.growthrate = ((self.daily_conf[-1]/self.daily_conf[-2]) - 1)*100
+#
+#print(self.daily_conf)
+#print(self.conf)
+#
+#print((self.conf[-1]/self.conf[-2]))
+#
+#print(self.growthrate)
+
+#%%
+#
+#kerala = State(df,'kl')
+#kerala.get_details()
+#kerala.plot_summary()
+
 #state = 'kl'
 #Filter = df[(df['value.state'] == state)]
 #
